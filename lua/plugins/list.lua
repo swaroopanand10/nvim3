72e4ca4834a35c317edd14e149097ad119516a05
swaroopanand <swaroopanand@email.com>
2024-05-26 22:57:42 +0530

    removed comment plugin since comments are now native in neovim

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -399,29 +399,29 @@ local plugins = {
             },
         },
     },
-    {
-        'echasnovski/mini.comment',
-        version = false,
-        keys = {
-            {
-                'gcc',
-                mode = { 'n' },
-                function()
-                    require('Comment').toggle()
-                end,
-                desc = 'Comment',
-            },
-            {
-                'gc',
-                mode = { 'v' },
-                function()
-                    require('Comment').toggle()
-                end,
-                desc = 'Comment',
-            },
-        },
-        config = true,
-    },
+    -- {
+    --     'echasnovski/mini.comment',
+    --     version = false,
+    --     keys = {
+    --         {
+    --             'gcc',
+    --             mode = { 'n' },
+    --             function()
+    --                 require('Comment').toggle()
+    --             end,
+    --             desc = 'Comment',
+    --         },
+    --         {
+    --             'gc',
+    --             mode = { 'v' },
+    --             function()
+    --                 require('Comment').toggle()
+    --             end,
+    --             desc = 'Comment',
+    --         },
+    --     },
+    --     config = true,
+    -- },
     {
         'nvim-neorg/neorg',
         -- build = ':Neorg sync-parsers',
